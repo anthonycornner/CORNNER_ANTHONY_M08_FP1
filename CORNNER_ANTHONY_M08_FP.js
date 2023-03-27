7b321d4b46d5d824675dae11de01eeed92a8bd14
anthonycornner <cornneranthony@gmail.com>
2023-03-26 23:08:17 -0400

    first commit

---
+++ CORNNER_ANTHONY_M08_FP.js
@@ -0,0 +1,7 @@
+// Skip's Fitness Center: Who's Training Who? | Final Project
+// Anthony Cornner
+// Version 1.0.0 3/26/2023
+// This program will take an input of a trainer's last name and the number of new enrollees the trainer has recruited. It will output the number of trainers who have enrolled members in three categories: 0-5 new members, 6-10 new members, and 11-15 new members. No trainer can enroll more than 15 new members. The program allows the user to enter 10 trainer's names and the number of members they have enrolled into an array, and it will traverse the array in order to generate the output.
+// Variables used: 
+
+
